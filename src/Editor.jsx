5dc607e123b9ce26532d3279a23f0a1e8f46312a
Zhi Wang <zwang@cs.fsu.edu>
2022-03-25 20:33:21 -0400

    add css

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import CodeMirror from "@uiw/react-codemirror";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
@@ -5,14 +6,28 @@ import { snazzy } from "./Theme";
 import { useState } from "react";
 import { mdCode } from "./Initcode";
 import { autocompletion } from "@codemirror/autocomplete";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import MarkdownPreview from "@uiw/react-markdown-preview";
-import Paper from "@mui/material/Paper";
-import useWindowSize from "./WindowHook";
 import mermaid from "mermaid";
+import "./viewer.css";
 
-const tableTemplate = `| Syntax      | Description | Test Text     |
+const Container = styled.div`
+	display: flex;
+	width: 100vw;
+	height: 100vh;
+`;
+
+const LeftPanel = styled.div`
+	flex: 1;
+	overflow: auto;
+`;
+
+const RightPanel = styled.div`
+	flex: 1;
+	overflow: auto;
+`;
+
+const tableTemplate = `
+| Syntax      | Description | Test Text     |
 | :---        |    :----:   |          ---: |
 | Header      | Title       | Here's this   |
 | Paragraph   | Text        | And more      |
@@ -23,7 +38,8 @@ const taskTemplate = `- [x] step 1
 - [ ] step 3: profit
 `;
 
-const codeTemplate = `\`\`\`go
+const codeTemplate = `
+\`\`\`go
   package main
   import "fmt"
   func main() {
@@ -44,140 +60,137 @@ stateDiagram-v2
 `;
 
 function mdCompletion(context) {
-    // match any string that starts with a / and followed by characters.
-    let word = context.matchBefore(/\/\w*/);
+	// match any string that starts with a / and followed by characters.
+	let word = context.matchBefore(/\/\w*/);
 
-    if (!word || (word.from === word.to && !context.explicit)) {
-        return null;
-    }
+	if (!word || (word.from === word.to && !context.explicit)) {
+		return null;
+	}
 
-    return {
-        from: word.from,
-        options: [
-            { label: "/h1", type: "text", apply: "\n# " },
-            { label: "/h2", type: "text", apply: "\n## " },
-            { label: "/h3", type: "text", apply: "\n### " },
-            { label: "/bold", type: "text", apply: "**bold text**" },
-            { label: "/italic", type: "text", apply: "_italic text_" },
-            {
-                label: "/code2",
-                type: "text",
-                apply: '```printf ("hello world!");```',
-                detail: "inline code",
-            },
-            {
-                label: "/code",
-                type: "text",
-                apply: codeTemplate,
-                detail: "code block",
-            },
-            {
-                label: "/mermaid",
-                type: "text",
-                apply: mermaidTemplate,
-                detail: "diagram",
-            },
-            { label: "/quote", type: "text", apply: "\n> text here!\n>" },
-            {
-                label: "/list",
-                type: "text",
-                apply: "\n1. \n2. \n3.",
-                detail: "ordered",
-            },
-            {
-                label: "/list2",
-                type: "text",
-                apply: "\n- \n- \n-",
-                detail: "unordered",
-            },
-            { label: "/link", type: "text", apply: "[text](url)" },
-            { label: "/image", type: "text", apply: "![text](url)" },
-            {
-                label: "/image2",
-                type: "text",
-                apply: '<img src="url" width="800"/>',
-                detail: "with size",
-            },
-            { label: "/table", type: "text", apply: tableTemplate },
-            { label: "/task", type: "text", apply: taskTemplate },
-        ],
-    };
+	return {
+		from: word.from,
+		options: [
+			{ label: "/h1", type: "text", apply: "\n# " },
+			{ label: "/h2", type: "text", apply: "\n## " },
+			{ label: "/h3", type: "text", apply: "\n### " },
+			{ label: "/bold", type: "text", apply: "**bold text**" },
+			{ label: "/italic", type: "text", apply: "_italic text_" },
+			{
+				label: "/code2",
+				type: "text",
+				apply: '```printf ("hello world!");```',
+				detail: "inline code",
+			},
+			{
+				label: "/code",
+				type: "text",
+				apply: codeTemplate,
+				detail: "code block",
+			},
+			{
+				label: "/mermaid",
+				type: "text",
+				apply: mermaidTemplate,
+				detail: "diagram",
+			},
+			{ label: "/quote", type: "text", apply: "\n> text here!\n>" },
+			{
+				label: "/list",
+				type: "text",
+				apply: "\n1. \n2. \n3.",
+				detail: "ordered",
+			},
+			{
+				label: "/list2",
+				type: "text",
+				apply: "\n- \n- \n-",
+				detail: "unordered",
+			},
+			{ label: "/link", type: "text", apply: "[text](url)" },
+			{ label: "/image", type: "text", apply: "![text](url)" },
+			{
+				label: "/image2",
+				type: "text",
+				apply: '<img src="url" width="800"/>',
+				detail: "with size",
+			},
+			{ label: "/table", type: "text", apply: tableTemplate },
+			{ label: "/task", type: "text", apply: taskTemplate },
+		],
+	};
 }
 
 // A component to markdown previewer to handle mermaid diagrams
 const getCode = (arr = []) =>
-    arr
-        .map((dt) => {
-            if (typeof dt === "string") {
-                return dt;
-            }
+	arr
+		.map((dt) => {
+			if (typeof dt === "string") {
+				return dt;
+			}
 
-            if (dt.props && dt.props.children) {
-                return getCode(dt.props.children);
-            }
+			if (dt.props && dt.props.children) {
+				return getCode(dt.props.children);
+			}
 
-            return "";
-        })
-        .filter(Boolean)
-        .join("");
+			return "";
+		})
+		.filter(Boolean)
+		.join("");
 
 const mermaidComponet = ({ inline, children = [], className, ...props }) => {
-    const code = getCode(children);
-    if (
-        typeof code === "string" &&
-        typeof className === "string" &&
-        /^language-mermaid/.test(className.toLocaleLowerCase())
-    ) {
-        const Elm = document.createElement("div");
-        Elm.id = "demo";
-        const svg = mermaid.render("demo", code);
-        return <code dangerouslySetInnerHTML={{ __html: svg }} />;
-    }
-    return <code className={String(className)}>{children}</code>;
+	const code = getCode(children);
+	if (
+		typeof code === "string" &&
+		typeof className === "string" &&
+		/^language-mermaid/.test(className.toLocaleLowerCase())
+	) {
+		const Elm = document.createElement("div");
+		Elm.id = "demo";
+		const svg = mermaid.render("demo", code);
+		return <code dangerouslySetInnerHTML={{ __html: svg }} />;
+	}
+	return <code className={String(className)}>{children}</code>;
 };
 
 function MdEditor() {
-    const [content, setContent] = useState(mdCode);
-    const { height } = useWindowSize({ width: 0, height: 8 });
+	const [content, setContent] = useState(mdCode);
+	// const { height } = useWindowSize({ width: 0, height: 8 });
 
-    const onChange = (value, viewUpdate) => {
-        setContent(value);
-    };
+	const onChange = (value, viewUpdate) => {
+		setContent(value);
+	};
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Paper>
-                        <CodeMirror
-                            value={mdCode}
-                            theme={snazzy}
-                            onChange={onChange}
-                            height={height + "px"}
-                            basicSetup
-                            extensions={[
-                                markdown({
-                                    base: markdownLanguage,
-                                    codeLanguages: languages,
-                                }),
-                                autocompletion({ override: [mdCompletion] }),
-                            ]}
-                        />
-                    </Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper sx={{ height: { height }, overflow: "auto" }}>
-                        <MarkdownPreview
-                            style={{ padding: 16 }}
-                            source={content}
-                            components={{ code: mermaidComponet }}
-                            linkTarget="_blank"
-                        />
-                    </Paper>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+	return (
+		<Container>
+			<LeftPanel>
+				<CodeMirror
+					value={mdCode}
+					theme={snazzy}
+					onChange={onChange}
+					height={`calc(100vh)`}
+					basicSetup
+					extensions={[
+						markdown({
+							base: markdownLanguage,
+							codeLanguages: languages,
+						}),
+						autocompletion({
+							override: [mdCompletion],
+						}),
+					]}
+				/>
+			</LeftPanel>
+
+			<RightPanel>
+				<MarkdownPreview
+					style={{ padding: 16 }}
+					source={content}
+					components={{ code: mermaidComponet }}
+					linkTarget="_blank"
+				/>
+			</RightPanel>
+		</Container>
+	);
 }
 
 export default MdEditor;
